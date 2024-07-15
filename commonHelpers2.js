be34cd157ea7b9c2839b91287545dc964da4c0b8
sssBu4 <102925509+sssBu4@users.noreply.github.com>
2024-07-16 01:26:10 +0300

    Initialize gh-pages

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{i as o}from"./assets/vendor-77e16229.js";const i=document.querySelector(".form");i.addEventListener("submit",e=>{e.preventDefault();const s=parseInt(i.elements.delay.value),r=i.elements.state.value;n(s,r).then(t=>{o.success({title:"Success",message:`✅ Fulfilled promise in ${t}ms`})}).catch(t=>{o.error({title:"Error",message:`❌ Rejected promise in ${t}ms`})})});function n(e,s){return new Promise((r,t)=>{setTimeout(()=>{s==="fulfilled"?r(e):t(e)},e)})}
+//# sourceMappingURL=commonHelpers2.js.map
